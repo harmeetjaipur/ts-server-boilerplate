8bd966bc5fcba3674db6477b74928bcba816e12b
Harmeet Singh <hsingh@nobul.com>
2019-06-10 11:53:27 -0400

    add middleware and route boilerplate

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
+import configureEnv from "./utils/configure-env";
+configureEnv();
+
 import http from "http";
 import express from "express";
 
+// Middlewares:
+// - adding more middlewares requires no change here
+// - create new middleware(s) under ./middleware and import it in ./middleware/index.ts
+import { applyMiddleware, applyRoutes } from "./utils";
+import middleware from "./middleware";
+
+// Routes:
+// - add new functionality under ./service directory
+// - import their routes in ./service/index.ts to add new routes
+import routes from "./services";
+
 const router = express();
+applyMiddleware(middleware, router);
+applyRoutes(routes, router);
+
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
+
+console.log(process.env.NODE_ENV);
 
 server.listen(PORT, () =>
   console.log(`Server is running http://localhost:${PORT}...`)
---
+++ src/utils/configure-env.ts
@@ -0,0 +1,20 @@
+import dotenv, { config } from "dotenv";
+import fs from "fs";
+
+const configure = (): void => {
+  // config();
+  const ENV_FILE = `.env.${process.env.NODE_ENV}`;
+
+  try {
+    const envConfig = dotenv.parse(fs.readFileSync(ENV_FILE));
+
+    Object.keys(envConfig).forEach(key => {
+      process.env[key] = envConfig[key];
+    });
+  } catch (error) {
+    console.log(`Could not open environtment file ${ENV_FILE}`);
+    process.exit(1);
+  }
+};
+
+export default configure;
